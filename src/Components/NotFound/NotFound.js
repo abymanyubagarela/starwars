08fefb8e4d65d017d7e3d742ba034b9771a0df70
abymanyubagarela <abymanyubagarela@gmail.com>
2020-10-22 04:08:22 +0700

    adding 404 image

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './NotFound.scss';
+import notfoundimage from '../../Assets/images/404.svg';
 
 class NotFound extends Component {
-    render() {
-        return (
-            <div className="stars">
-                <div className="central-body">
-                    <img className="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" alt=""/>
-                    <a href="/" className="btn-go-home">GO BACK HOME</a>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='stars'>
+        <div className='central-body'>
+          <img className='image-404' src={notfoundimage} width='300px' alt='' />
+          <a href='/' className='btn-go-home'>
+            GO BACK HOME
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotFound;
