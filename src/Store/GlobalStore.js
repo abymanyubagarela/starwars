b5feb70f49628e48f68afbc3d7fb43517f7c75b0
abymanyubagarela <abymanyubagarela@gmail.com>
2020-10-22 07:08:13 +0700

    apply middleware devtools

--- src/Store/GlobalStore.js
+++ src/Store/GlobalStore.js
@@ -10,8 +10,7 @@ const globalStore = () =>
     initialState,
     compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
   );
 
